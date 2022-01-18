8926b7c400eaa97f93f9227cda354c7a6e901af2
JDr-Etnetera <56111669+JDr-Etnetera@users.noreply.github.com>
2022-01-18 21:43:36 +0100

    Turn Angle (multiples of 45)

--- main.ts
+++ main.ts
@@ -1,3 +1,167 @@
+function logValue(key: string, value: number) {
+    if (SERIAL_LOGGING) {
+        serial.writeValue(key, value)
+    }
+}
+
+class CorrectionMap {
+    private maxKeys: number;
+    private maxEntities: number;
+    private map: number[][]; // angle index & values count
+    private angleList: number[];
+    /**
+     * @keys        max number of different angles
+     * @entities    max number of all records
+     */
+    constructor(keys: number, entities: number) {
+        this.maxKeys = keys; //unused right now
+        this.maxEntities = entities;
+        this.reset();
+    }
+    private isNewAngle(angle: number) {
+        return (this.angleList.indexOf(angle) < 0)
+    }
+    /**
+     * Adds angle to history
+     */
+    addAngle(angle: number) {
+        if (this.isNewAngle(angle)) {
+            this.map[angle] = [];
+            this.map[angle][0] = 0;
+            this.angleList.push(angle);
+        }
+        this.map[angle][0] = this.map[angle][0] + 1;
+    }
+    private xxx(item: number[], index: number) {
+
+    }
+    /**
+     * Removes angle from history
+     */
+    removeAngle(angle: number) {
+        console.log("removeAngle(" + angle + ")")
+        if (this.isNewAngle(angle)) {
+            console.log("remAngle> new angle - this should not happen")
+            return false;
+        }
+
+        console.log("remAngle(" + angle + ")> subtrackt angle");
+        this.map[angle][0] = this.map[angle][0] - 1;
+        try {
+            if (this.map[angle][0] == 0) {
+                console.log("remAngle> final cleanup");
+                //this.map.splice(angle-1, 1)
+                /*this.map.forEach(function (value: number[], index: number) {
+                    tmp = NaN;
+                    if ( (!! this.map[index]) && this.map[index][0] !== NaN) {
+                        tmp = this.map[index][0]
+                    }
+                    console.log("check> [" + index + "]:" + (isNaN(tmp) ? "" : tmp))
+                })*/
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        return true;
+    }
+    /**
+     * Deletes all records of given angle
+     */
+    removeAngleHistory(angle: number) {
+        this.map[angle][0] = 0;
+    }
+    /**
+     * Deletes all records collected
+     */
+    reset() {
+        this.map = [];
+        this.angleList = [];
+    }
+    getAll() {
+        return this.map;
+    }
+    /**
+     * Returns list of collected angles
+     */
+    getKnownAngles() {
+        return this.angleList;
+    }
+}
+
+class CompassHistory {
+    private maxEntries: number;
+    private compassTick: number;
+    private callibrationTime: number;
+    private history: number[];
+    private correction: CorrectionMap;
+    /**
+     * @max compass calibration history length
+     * @tick how long wait before repeating reading from compass [ms]
+     */
+    constructor(max: number, tick: number) {
+        this.history = [];
+        this.maxEntries = max;
+        this.compassTick = tick;
+        this.callibrationTime = max * tick;
+        this.correction = new CorrectionMap(10, 20);
+    }
+    /**
+     * Adds angle to history tracking
+     */
+    append(angle: number) {
+        logValue("c", angle)
+        if (this.history.length == this.maxEntries) {
+            this.correction.removeAngle(this.history[0]);
+            this.history.splice(0, this.history.length - this.maxEntries + 1);
+        }
+        this.history.push(angle);
+        this.correction.addAngle(angle);
+    }
+    /**
+     * Returns angle with correction
+     * @dec number of decimal places
+     */
+    getDelta(dec: number) {
+        if (this.getLength() < 1) return -100;
+        let sum = 0;
+        this.history.forEach(function (e) {
+            sum += e;
+        });
+        let delta2 = Math.round((sum * Math.pow(10, dec)) / this.getLength()) / Math.pow(10, dec);
+        logValue("d", delta2)
+        return delta2;
+    }
+    fullCalibration() {
+        this.reset()
+        for (let j = 0; j < this.maxEntries; j++) {
+            this.append(input.compassHeading())
+            basic.pause(this.compassTick)
+        }
+    }
+    getFromRow() {
+        if (this.history.length > 0) {
+            return this.history.slice(0, 1);
+        }
+        return -100;
+    }
+    getLength() {
+        return this.history.length;
+    }
+    reset() {
+        this.history = [];
+        this.correction.reset();
+    }
+    getHistory() {
+        return this.history
+    }
+    getCorrection() {
+        return this.correction.getAll();
+    }
+    getKnownAngles() {
+        return this.correction.getKnownAngles();
+    }
+}
+
 class DrivingSection {
     private key: number
     private leftWheelSpd: number
@@ -39,13 +203,17 @@ class ShenandoahCar {
     private D005_QT_TIME = 225
 
     private directDriveSections = [{}]
+    private clockwiseSections = [{}]
+    private anticlockwiseSections = [{}]
     
     /* Rotation */
 
     private LEFT_ROTATION_SPD_MAX = 50
     private RIGHT_ROTATION_SPD_MAX = -46
-    private A090_MS_TIME = 850
-    private A045_MS_TIME = 1000
+    private A90_MS_CLOCK = 840
+    private A45_MS_CLOCK = 475
+    private A90_MS_ANTI = 750
+    private A45_MS_ANTI = 460
 
     private initDrivingSections() {
         let tmpArray  = []
@@ -53,14 +221,18 @@ class ShenandoahCar {
         tmpArray[50]  = new DrivingSection( 50, this.LEFT_HALF_SPD, this.RIGHT_HALF_SPD, this.D050_HS_TIME)
         tmpArray[20]  = new DrivingSection( 20, this.LEFT_HALF_SPD, this.RIGHT_HALF_SPD, this.D020_HS_TIME)
         tmpArray[10]  = new DrivingSection( 10, this.LEFT_QUARTER_SPD, this.RIGHT_QUARTER_SPD, this.D010_QT_TIME)
-        tmpArray[5]   = new DrivingSection(  5, this.LEFT_QUARTER_SPD, this.RIGHT_QUARTER_SPD, this.D005_QT_TIME)
-        /*tmpArray = [
-            { key: 100, left: this.LEFT_HALF_SPD, right: this.RIGHT_HALF_SPD, time: this.D100_HS_TIME },
-            { key: 50, left: this.LEFT_HALF_SPD, right: this.RIGHT_HALF_SPD, time: this.D050_HS_TIME },
-            { key: 20, left: this.LEFT_HALF_SPD, right: this.RIGHT_HALF_SPD, time: this.D020_HS_TIME },
-            { key: 10, left: this.LEFT_HALF_SPD, right: this.RIGHT_HALF_SPD, time: this.D010_HS_TIME }
-        ]*/
+        tmpArray[5]   = new DrivingSection(  5, this.LEFT_QUARTER_SPD, this.RIGHT_QUARTER_SPD, this.D005_QT_TIME)        
         this.directDriveSections = tmpArray.sort((first, second) => 0 - (first.getKey() > second.getKey() ? 1 : -1))
+
+        tmpArray = []
+        tmpArray[90] = new DrivingSection(90, this.LEFT_ROTATION_SPD_MAX, this.RIGHT_ROTATION_SPD_MAX, this.A90_MS_CLOCK)
+        tmpArray[45] = new DrivingSection(45, this.LEFT_ROTATION_SPD_MAX, this.RIGHT_ROTATION_SPD_MAX, this.A45_MS_CLOCK)
+        this.clockwiseSections = tmpArray.sort((first, second) => 0 - (first.getKey() > second.getKey() ? 1 : -1))
+
+        tmpArray = []
+        tmpArray[90] = new DrivingSection(90, -1 * this.LEFT_ROTATION_SPD_MAX, -1 * this.RIGHT_ROTATION_SPD_MAX, this.A90_MS_ANTI)
+        tmpArray[45] = new DrivingSection(45, -1 * this.LEFT_ROTATION_SPD_MAX, -1 * this.RIGHT_ROTATION_SPD_MAX, this.A45_MS_ANTI)
+        this.anticlockwiseSections = tmpArray.sort((first, second) => 0 - (first.getKey() > second.getKey() ? 1 : -1))
     }
 
     constructor() {
@@ -81,7 +253,49 @@ class ShenandoahCar {
         })
     }
 
-    private driveSection(distance: number, section: DrivingSection) { 
+    /**
+     * Rotates by a given angle
+     * @angle in [deg] (right now there are supported only multiples of 45°)
+     */
+    turn(angle: number) {        
+        /*if (angle == 90) {
+            RingbitCar.freestyle(50, -46)
+            basic.pause(840)
+            RingbitCar.brake()
+            basic.pause(200)
+        } else if (angle == -90) {
+            RingbitCar.freestyle(-50, 46)
+            basic.pause(750)
+            RingbitCar.brake()
+            basic.pause(200)
+        } else if (angle == 45) {
+            RingbitCar.freestyle(50, -46)
+            basic.pause(475)
+            RingbitCar.brake()
+            basic.pause(200)
+        } else if (angle == -45) {
+            RingbitCar.freestyle(-50, 46)
+            basic.pause(460)
+            RingbitCar.brake()
+            basic.pause(200)
+        } else {
+            basic.showIcon(IconNames.No)
+        }*/
+        let rest = 0
+        let sections = [{}]
+        if (angle == 0) {return}
+        if (angle > 0) {
+            sections = this.clockwiseSections
+            rest = angle
+        } else {
+            sections = this.anticlockwiseSections
+            rest = -1 * angle
+        }
+        sections.forEach(function (section: DrivingSection, key: number) {
+            rest = this.driveSection(rest, section)
+        })
+    }
+    private driveSection(distance: number, section: DrivingSection) {
         let numSections = Math.floor(distance / section.getKey())
         let rest = distance % section.getKey()
         console.log("[" + section.getKey() + "]: " + numSections + " (" + rest + ")")
@@ -94,43 +308,10 @@ class ShenandoahCar {
         }
         return rest
     }
-
-    /**
-     * Rotates by a given angle
-     * @angle in [deg] right now only supported angles {-45,45,-90,90}
-     */
-    turn(angle: number) {        
-        if (angle == 90) {
-            RingbitCar.freestyle(50, -46)
-            basic.pause(840)
-            RingbitCar.brake()
-            basic.pause(200)
-        } else if (angle == -90) {
-            RingbitCar.freestyle(-50, 46)
-            basic.pause(840)
-            RingbitCar.brake()
-            basic.pause(200)
-        } else if (angle == 45) {
-            RingbitCar.freestyle(50, -46)
-            basic.pause(475)
-            RingbitCar.brake()
-            basic.pause(200)
-        } else if (angle == -45) {
-            RingbitCar.freestyle(-50, 46)
-            basic.pause(475)
-            RingbitCar.brake()
-            basic.pause(200)
-        } else {
-            basic.showIcon(IconNames.No)
-        }
-    }
-
-    private turnSection(angle:number) {
-
-    }
 }
 
 let car = new ShenandoahCar()
+let SERIAL_LOGGING = true;
 /*
 input.onButtonPressed(Button.A, function () {
     basic.showString("P")
@@ -153,19 +334,16 @@ input.onButtonPressed(Button.B, function () {
     RingbitCar.brake()
 })
 */
-
+/*
 input.onButtonPressed(Button.A, function () {
     car.drive(50)
-    RingbitCar.freestyle(-50, 46)
-    basic.pause(470)
-    RingbitCar.brake()
+    car.drive(135)
     car.drive(50)
 })
 
 input.onButtonPressed(Button.B, function () {
     car.drive(50)
-    RingbitCar.freestyle(-50, 46)
-    basic.pause(460)
-    RingbitCar.brake()
+    car.drive(-135)
     car.drive(50)
 })
+*/
